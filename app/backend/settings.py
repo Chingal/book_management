729857caaa70a800e71077baad4a6589e6783fcb
Chingal <rchingal@gmail.com>
2025-01-06 18:17:50 -0500

    Update: settings.py

--- app/backend/settings.py
+++ app/backend/settings.py
@@ -105,7 +105,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),  # Verifica que este directorio exista
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
 
